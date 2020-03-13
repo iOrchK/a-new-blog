19af92ffdc403a335682ba0c302bcd2a30482bce
Jorge Chable <yahicimosclick9193@hotmail.com>
2020-03-13 00:40:21 -0600

    test plugin 1
    test of composer require jc-plugins/jc-plugins

--- wp-content/plugins/jc-plugins/README.md
+++
@@ -1,2 +0,0 @@
-# a-new-blog
-Proyecto generado para el desarrollo de plugins en un blog de WordPress.
--- wp-content/plugins/jc-plugins/jc-plugins.php
+++ wp-content/plugins/jc-plugins/jc-plugins.php
@@ -9,84 +9,8 @@
      * Author URI: http://iorchk.com/
      * Licence: GPL2
      */
+    require("vendor/jc-plugins/jc-plugins/jc-plugins.php");
 
-    // add_filter("the_content", "jc_getUserById");
-    add_filter("the_content", "jc_getUserList");
-
-    /**
-     * Change the content in all pages and
-     * display an user list by http get from the API Url of https://jsonplaceholder.typicode.com/users
-     */
-    if (!function_exists("jc_getUserList"))
-    {
-        function jc_getUserList($content)
-        {
-            if (is_singular("post"))
-            {
-                return $content;
-            }
-
-            $json = wp_remote_get("https://jsonplaceholder.typicode.com/users");
-            $p_userList = json_decode($json["body"]);
-            $content = "
-                <h3>JC Plugin Activated</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>User Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-            ";
-
-            foreach ($p_userList as $user)
-            {
-                $content .= "
-                    <tr title='Click to view detail'>
-                        <th><a href='<?php jc_getUserById({$user->id}); ?>'>{$user->id}</a></th>
-                        <td><a href='<?php jc_getUserById({$user->id}); ?>'>{$user->name}</a></td>
-                        <td><a href='<?php jc_getUserById({$user->id}); ?>'>{$user->username}</a></td>
-                    </tr>
-                ";
-            }
-
-            $content .= "
-                    </tbody>
-                </table>
-            ";
-
-            return $content;
-        }
-    }
-
-    /**
-     * Change the content in all pagesa and
-     * display the user detail by the user at the list that was made clic by http get from 
-     * the API Url of https://jsonplaceholder.typicode.com/users
-     */
-    if (!function_exists("jc_getUserById"))
-    {
-        function jc_getUserById($id) {
-            $json = wp_remote_get("https://jsonplaceholder.typicode.com/users/" . $id);
-            $p_user = json_decode($json["body"]);
-            $content = "
-                <p>Detail:</p>
-                <dl>
-                    <dt>Id:</dt>
-                    <dd>{$p_user->id}</dd>
-
-                    <dt>Name:</dt>
-                    <dd>{$p_user->name}</dd>
-
-                    <dt>User Name:</dt>
-                    <dd>{$p_user->username}</dd>
-                </dl>
-            ";
-
-            return $content;
-        }
-    }
-    
+    // $JcPluginInstance = new JcPluginClass;
+    // $JcPluginInstance->init();
 ?>
